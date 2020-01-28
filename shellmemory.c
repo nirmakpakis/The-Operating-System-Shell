76e5f52363b8870cafd2e76d66ff44641524f618
Irmak Parkis <nihal.pakis@mail.mcgill.ca>
2020-01-28 14:17:51 -0500

    Memory implemented :art:

--- shellmemory.c
+++ shellmemory.c
@@ -1,3 +1,69 @@
+/**
+* Nihal Irmak Pakis
+* 260733837
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct node
+{
+    char *key;
+    char *value;
+    struct node *next;
+} node;
+
+node *head = NULL;
+
+bool isEmpty()
+{
+    return head == NULL;
+}
+
+// Find key
+node *find(char *key)
+{
+    if (isEmpty())
+        return NULL;
+    node *current = head;
+    while (strcmp(current->key, key) != 0)
+    {
+        if (current->next == NULL)
+            return NULL;
+        current = current->next;
+    }
+    return current;
+}
+
+// Add node
+void insert(char *key, char *value)
+{
+    if (find(key) == NULL)
+    {
+        node *newNode = (node *)malloc(sizeof(node));
+        newNode->key = strdup(key);
+        newNode->value = strdup(value);
+        newNode->next = head;
+        head = newNode;
+    }
+    else
+    {
+        node *aNode = find(key);
+        aNode->value = strdup(value);
+    }
+}
+
+// Print memory
+void print()
+{
+    node *ptr = head;
+    printf("\n[ ");
+    while (ptr != NULL)
+    {
+        printf("(%s,%s) \n", strdup(ptr->key), strdup(ptr->value));
+        ptr = ptr->next;
+    }
+    printf(" ]");
+}
